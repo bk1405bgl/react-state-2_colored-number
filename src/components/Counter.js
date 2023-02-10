0e7c34b65e3d90089027ff0f78d0f126aa57273c
Bilal Kuzey <bk1405bgl@gmail.com>
2023-02-10 11:18:09 +0100

    imported useState and used useState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,8 @@
 import ColoredNumber from "./ColoredNumber";
+import { useState } from "react";
 
 export default function Counter() {
+  const [count, setCount] = useState(0);
   return (
     <div className="counter">
       <h1>Colored Number</h1>
